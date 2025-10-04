ceacaf64167f66b81cd1e3c5bfcebf60a002ac42
silverblade34 <mpacheco.tacay@gmail.com>
2025-10-03 23:17:11 -0500

    fix: se quito opciones question types

--- src/modules/learning/enum/question-type.enum.ts
+++ src/modules/learning/enum/question-type.enum.ts
@@ -2,8 +2,8 @@ export enum QuestionType {
   MULTIPLE_CHOICE = 'multiple_choice',
   MULTIPLE_SELECT = 'multiple_select',
   TRUE_FALSE = 'true_false',
-  FILL_BLANK = 'fill_blank',
-  DRAG_DROP = 'drag_drop',
-  SEQUENCE_ORDER = 'sequence_order',
+  // FILL_BLANK = 'fill_blank',
+  // DRAG_DROP = 'drag_drop',
+  // SEQUENCE_ORDER = 'sequence_order',
   SELECT_TEXT = 'select_text',
 }
